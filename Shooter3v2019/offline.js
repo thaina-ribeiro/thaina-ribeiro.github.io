a112b4cd211d062f9b162d1138f480c68bee6ebe
thaina-ribeiro <50911560+thaina-ribeiro@users.noreply.github.com>
2019-05-22 08:08:18 -0300

    Add files via upload

---
+++ Shooter3v2019/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1558523177,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/atirarbotão-sheet0.png",
+		"images/atirarbotão-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/setacima-sheet1.png",
+		"images/setadireita-sheet0.png",
+		"images/setadireita-sheet1.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setaesquerda-sheet1.png",
+		"images/setabaixo-sheet0.png",
+		"images/setabaixo-sheet1.png",
+		"images/fundo-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/destruirinimigos-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
