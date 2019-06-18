a851d3c6336cf29c8aed1d938bb2dfde47167a10
thaina-ribeiro <50911560+thaina-ribeiro@users.noreply.github.com>
2019-06-17 21:10:44 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1560815877,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/setabaixo-sheet0.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setadireita-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/botaogay-sheet0.png",
+		"images/hp.png",
+		"images/vermelhinho-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/tiledbackground.png",
+		"images/inimigocoacisco-sheet0.png",
+		"images/gravidade-sheet0.png",
+		"images/sensor-sheet0.png",
+		"images/fundo.png",
+		"images/poerup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
